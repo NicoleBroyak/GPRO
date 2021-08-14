5d688190b634d29724dd4c6dacf46a6c0c03eab5
Nikola Broyak <nikolabroyak@gmail.com>
2021-08-14 11:43:02 +0200

    BestFans update code with "with" and endoding utf-8

--- main.py
+++ main.py
@@ -1,22 +1,25 @@
 import requests
 from selenium import webdriver
 from bs4 import BeautifulSoup
-import mechanize
-import urllib3
-import http.cookiejar
+import pathlib
+import logging
 
 
 def best_cars():
     season = "83"
     race = "3"
     lvl = 0
-    file = open("BestCars.txt", "w")
-    file.write("")
-    file.close()
+    file_path = pathlib.Path("BestCars.csv")
+    try:
+        with file_path.open(mode="w") as file:
+            file.write("")
+    except OSError:
+        logging.error("Error11")
     url = "https://gpro.net/pl/Stats.asp?type=bestcars&Page=1"
     page = requests.get(url)
     soup = BeautifulSoup(page.content, "html.parser")
     max_page = soup.find_all("u")
+    max_page_number = None
     for u in max_page:
         max_page_number = u
     max_pageno = int(max_page_number.text.strip()) + 1
@@ -37,36 +40,46 @@ def best_cars():
             if count == 0:
                 # print(season + '\t')
                 count += 1
-                file = open("BestCars.txt", "a")
-                file.write('{}\t'.format(season))
-                file.close()
+                try:
+                    with file_path.open(mode="a") as file:
+                        file.write(f"{season}")
+                except OSError:
+                    logging.error("Error")
             if count == 1:
                 # print(race + '\t')
                 count += 1
-                file = open("BestCars.txt", "a")
-                file.write('{}\t'.format(race))
-                file.close()
+                try:
+                    with file_path.open(mode="a") as file:
+                        file.write(f"{race}")
+                except OSError:
+                    logging.error("Error")
             if count <= 5:
-                file = open("BestCars.txt", "a")
-                file.write(tr.text.strip())
-                file.write("\t")
-                file.close()
+                try:
+                    with file_path.open(mode="a", encoding="utf-8") as file:
+                        file.write(tr.text.strip())
+                        if count != 4:
+                            file.write(",")
+                except OSError:
+                    logging.error("Error")
             if count == 6:
                 imgs = tr.find_all("img")
                 for imgs in tr:
                     lvl += 1
                 lvl -= 2
-                file = open("BestCars.txt", "a")
-                file.write(str(lvl))
-                file.write("\t")
-                file.close()
+                try:
+                    with file_path.open(mode="a") as file:
+                        file.write(str(lvl))
+                except OSError:
+                    logging.error("Error")
                 lvl = 0
             count += 1
             countall += 1
             if count == 7:
-                file = open("BestCars.txt", "a")
-                file.write("\n")
-                file.close()
+                try:
+                    with file_path.open(mode="a") as file:
+                        file.write("\n")
+                except OSError:
+                    logging.error("Error")
                 count = 0
         print(page_no)
         page_no += 1
@@ -758,10 +771,11 @@ def money_levels():
     file = open("MoneyLevels.txt", "w")
     user = input("User")
     password = input("Pass")
-    file.write('Sezon\tWyścig\tKlasa\tGrupa\t#\t\tNazwisko menadżera\tNazwisko '
-               'kierowcy\tOW\tPensja\tDługość\tNazwisko dyr technicznego\tOW\t'
-               'Pensja\tDługość\tOW Personelu\n')
-    driver = webdriver.Chrome()
+    driver = input("[F] - Firefox, [C] - Chrome")
+    if driver == "F":
+        driver = webdriver.Firefox()
+    if driver == "C":
+        driver = webdriver.Chrome()
     driver.get("https://gpro.net/pl/Login.asp?Redirect=MoneyLevels.asp")
     driver.find_element_by_name("textLogin").send_keys(user)
     driver.find_element_by_name("textPassword").send_keys(password)
